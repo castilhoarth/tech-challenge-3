b401fb0e82b374dbff125cd0ce2b91daed0a7220
Gerusa Lobo <gerusalobo@gmail.com>
2026-07-11 12:49:30 -0300

    targeting

--- test2.sh
+++ test2.sh
@@ -6,7 +6,7 @@
 
 BASE_URL_AUTH=${BASE_URL_AUTH:-http://acc28ae7dcc21487e87cdd1a2bbeb3d2-106617744.us-east-1.elb.amazonaws.com/auth}
 BASE_URL_FLAG=${BASE_URL_FLAG:-http://acc28ae7dcc21487e87cdd1a2bbeb3d2-106617744.us-east-1.elb.amazonaws.com/flags}
-BASE_URL_TARGETING=${BASE_URL_TARGETING:-http://acc28ae7dcc21487e87cdd1a2bbeb3d2-106617744.us-east-1.elb.amazonaws.com.8003}
+BASE_URL_TARGETING=${BASE_URL_TARGETING:-http://acc28ae7dcc21487e87cdd1a2bbeb3d2-106617744.us-east-1.elb.amazonaws.com/targeting}
 BASE_URL_EVALUATION=${BASE_URL_EVALUATION:-http://acc28ae7dcc21487e87cdd1a2bbeb3d2-106617744.us-east-1.elb.amazonaws.com:8004}
 BASE_URL_ANALYTICS=${BASE_URL_ANALYTICS:-http://acc28ae7dcc21487e87cdd1a2bbeb3d2-106617744.us-east-1.elb.amazonaws.com:8005}
 
@@ -23,10 +23,9 @@ echo "========================================"
 echo "AMBIENTE DE TESTE"
 echo "========================================"
 echo "AUTH      : $BASE_URL_AUTH"
-
-: <<'EOF'
 echo "FLAG      : $BASE_URL_FLAG"
 echo "TARGETING : $BASE_URL_TARGETING"
+: <<'EOF'
 echo "EVALUATION : $BASE_URL_EVALUATION"
 echo "ANALYTICS : $BASE_URL_ANALYTICS"
 EOF
@@ -45,15 +44,16 @@ curl "$BASE_URL_AUTH/health"
 echo
 echo
 
-: <<'EOF'
+
 curl "$BASE_URL_FLAG/health"
 echo
 echo
 
+
 curl "$BASE_URL_TARGETING/health"
 echo
 echo
-
+: <<'EOF'
 curl "$BASE_URL_EVALUATION/health"
 echo
 echo
@@ -91,7 +91,7 @@ echo "$API_KEY"
 echo ""
 echo ""
 
-: <<'EOF'
+
 ########################################
 # CRIAR FLAG
 ########################################
@@ -182,8 +182,6 @@ fi
 cat response.json
 
 echo ""
-echo ""
-
 echo "========================================"
 echo "7. Criando Regra de Targeting"
 echo "========================================"
@@ -256,7 +254,7 @@ cat response.json
 
 echo ""
 echo ""
-
+: <<'EOF'
 echo "========================================"
 echo "10. Testando a fila SQS e o processamento"
 echo "========================================"
